f102b4d127979a20c9565703b871bd65834fd706
Teagan Mucher <tjsmuch@gmail.com>
2018-08-25 15:00:48 -0700

    Edited css, added image

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -4,10 +4,19 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component {
   render() {
     return (
+      
+      // TODO: Change other links to link to other pages
       <header>
         <ul id="headerButtons">
           <li className="navButton">
             <Link to="">Home</Link>
+          </li>
+
+          <li className="navButton">
+            <Link to="">About</Link>
+          </li>
+          <li className="navButton">
+            <Link to="">Contact</Link>
           </li>
         </ul>
       </header>
