5c85f7d35a47e65526157e14e4518d94c3c6f784
Sara <sara.bawale@gmail.com>
2018-11-29 17:17:35 -0800

    Modified home page
    Made several changes to clean up homepage; currently using placeholder images

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -5,36 +5,28 @@ import logo from '../../Assets/oxyosLogo.jpg';
 class NavBar extends Component {
   render() {
     return (
-
       // TODO: Change other links to link to other pages
       <div className="header">
+          <span id="logoLink">
+          <Link to="/">
+            <img id="logoImage" src={logo}></img>
+          </Link>
+          </span>
 
-        <div>
-          <img id="logoImage" src={logo}></img>
-        </div>
-
-        <div>
           <ul id="headerButtons">
-
-            <li className="navButton">
-              <Link to="/events">Events</Link>
-            </li>
-
-            <li className="navButton">
-              <Link to="/resources">Resources</Link>
-            </li>
-
-            <li className="navButton">
-              <Link to="/about-us">About Us</Link>
-            </li>
-
             <li className="navButton">
               <Link to="/contact">Contact</Link>
             </li>
-
+            <li className="navButton">
+              <Link to="/about-us">About Us</Link>
+            </li>
+            <li className="navButton">
+              <Link to="/resources">Resources</Link>
+            </li>
+            <li className="navButton">
+              <Link to="/events">Events</Link>
+            </li>
           </ul>
-        </div>
-
       </div>
     )
   }
--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -3,10 +3,20 @@ import banner from '../Assets/2018-0608_JacarandaTrees-07.jpg';
 
 class HomePage extends Component {
   render() {
-    return (
-      <div>
-        <img id="bannerImage" src={banner}></img>
-      </div>
+    return (	
+	    <div id="homeBody" className="row">
+	    	<div id="homeTitle" className="column">
+		        <h1>Oxy</h1>
+		        <h1><span>Open</span></h1>
+		        <h1>Source</h1>
+	        </div>
+	        <div id="homeText" className="column">
+		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel pharetra sem, sit amet rutrum lorem. Vestibulum eu condimentum urna, eu aliquet tortor. Curabitur volutpat turpis nisi, sed porttitor turpis lobortis a. Mauris eu sapien turpis. Nam id orci blandit, venenatis orci vel, porta lacus. Sed sed blandit risus. Phasellus varius cursus eros, ut dictum ante ullamcorper at.
+		        </p>
+				<p>Morbi nec elit orci. Fusce consequat iaculis lacus ac pellentesque. Ut vel malesuada justo. Proin scelerisque lectus eget mauris tincidunt fringilla. Mauris non vulputate magna, vitae ullamcorper justo. Fusce sodales odio et metus suscipit, ut rutrum diam molestie. Integer porta, turpis eu molestie euismod, ex justo vehicula enim, et bibendum tellus mauris eget purus.
+				</p>
+			</div>
+	    </div>
     )
   }
 }
