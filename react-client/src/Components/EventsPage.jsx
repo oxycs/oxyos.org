5de10b0a4cd7e546234d61ccf76ef748ec3880fb
Teagan Mucher <tjsmuch@gmail.com>
2019-03-05 10:58:56 +0100

    Added calendar code that doesnt work yet

--- react-client/src/Components/EventsPage.jsx
+++ react-client/src/Components/EventsPage.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import Calendar from 'react_google_calendar'
 
 class EventsPage extends Component {
-  render() {
-    return (
-      <div>
-        <p>This is the events page</p>
-      </div>
-    )
+  // Credit: https://github.com/crashspringfield/react-google-calendar
+  const calendar_configuration = {
+      api_key: AIzaSyBrKrD2zCwme215QR0lh7TiCSg9ekkR5Is,
+      calendars: [
+        {
+          name: 'OxyOS Events', // whatever you want to name it
+          url: 'https://calendar.google.com/calendar/b/1?cid=b3h5LmVkdV82NDlzNmRjbGViNXIxMGtodGphdnIzZXM0b0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t' // your calendar URL
+        }
+      ],
+      dailyRecurrence: 700,
+      weeklyRecurrence: 500,
+      monthlyRecurrence: 20
+  }
+
+  export default class MyApp extends Component {
+      constructor(props) {
+        super(props)
+          this.state = {
+            events: []
+          }
+      }
+
+      render = () =>
+        <div>
+          <Calendar
+            events={this.state.events}
+            config={calendar_configuration} />
+        </div>
   }
 }
 
