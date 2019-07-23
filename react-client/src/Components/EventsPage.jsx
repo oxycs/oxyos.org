045d4c06a959e122d0febbba9578c9fbefc406c9
Teagan Mucher <tjsmuch@gmail.com>
2019-07-22 21:18:44 -0700

    Fixed image loading issues
    Still need to style background img

--- react-client/src/Components/EventsPage.jsx
+++ react-client/src/Components/EventsPage.jsx
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
-//import Calendar from 'react_google_calendar'
-import ApiCalendar from 'react-google-calendar-api';
 
 class EventsPage extends Component {
-  /**
-     * List all events in the calendar
-     * @param {number} maxResults to see
-     * @param {string} calendarId to see by default use the calendar attribute
-     * @returns {any} Promise with the result.
-     */
-  //public listUpcomingEvents(maxResults: number, calendarId: string = this.calendar): any
-
-  if (ApiCalendar.sign)
-    ApiCalendar.listUpcomingEvents(10)
-      .then(({result}: any) => {
-        console.log(result.items);
-      });
-
   render() {
     return (
       <div id="calendar">
@@ -24,47 +8,6 @@ class EventsPage extends Component {
       </div>
     )
   }
-
-
-  // Credit: https://github.com/crashspringfield/react-google-calendar
-  /*
-  const calendar_configuration = {
-      api_key: AIzaSyBrKrD2zCwme215QR0lh7TiCSg9ekkR5Is,
-      calendars: [
-        {
-          name: 'OxyOS Events', // whatever you want to name it
-          url: 'https://calendar.google.com/calendar/b/1?cid=b3h5LmVkdV82NDlzNmRjbGViNXIxMGtodGphdnIzZXM0b0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t' // your calendar URL
-        }
-      ],
-      dailyRecurrence: 700,
-      weeklyRecurrence: 500,
-      monthlyRecurrence: 20
-  }
-
-  export default class MyApp extends Component {
-      constructor(props) {
-        super(props)
-          this.state = {
-            events: []
-          }
-      }
-
-      render = () =>
-        <div>
-          <Calendar
-            events={this.state.events}
-            config={calendar_configuration} />
-        </div>
-  } */
-  /*
-  render() {
-    return (
-      <div id="calendar">
-        <p>This is the events page</p>
-      </div>
-    )
-  }
-  */
 }
 
 export default EventsPage;
--- react-client/src/Components/FooterComponent/Footer.jsx
+++ react-client/src/Components/FooterComponent/Footer.jsx
@@ -8,10 +8,10 @@ class Footer extends Component {
         <div id="social">
           <h1>Open<span>Source</span></h1>
           <h3>Occidental College</h3>
-          <a href="https://www.facebook.com/oxyopensourceclub"><img src='../../Assets/facebook.svg'></img></a>
-          <a href="https://www.instagram.com/oxyopensource/"><img src="../../Assets/instagram.svg"></img></a>
-          <a href="https://bit.ly/oxyos-slack-spring2019"><img src="../../Assets/slack.svg"></img></a>
-          <a href="https://github.com/oxy-os/oxyos.org"><img src="../../Assets/github.svg"></img></a>
+          <a href="https://www.facebook.com/oxyopensourceclub"><img src={require('../../Assets/facebook.jpg')}></img></a>
+          <a href="https://www.instagram.com/oxyopensource/"><img src={require('../../Assets/instagram.jpg')}></img></a>
+          <a href="https://bit.ly/oxyos-slack-spring2019"><img src={require('../../Assets/slack.jpg')}></img></a>
+          <a href="https://github.com/oxy-os/oxyos.org"><img src={require('../../Assets/github.jpg')}></img></a>
         </div>
         <div id="contact">
           <p>1600 Campus Road</p>
