9f333d6343224037d9659e439f93b5cbf275cf4c
Teagan Mucher <tjsmuch@gmail.com>
2019-07-31 20:06:21 -0700

    Fixed footer overlapping main content issue, aboutUs page changes

--- react-client/src/Components/AboutComponent/AboutUsPage.jsx
+++ react-client/src/Components/AboutComponent/AboutUsPage.jsx
@@ -6,35 +6,33 @@ class AboutUsPage extends Component {
   render() {
     return (
       <div class="main-container">
-        <section>
-          <section id="about-us">
+        <div id="about-us">
 
-            <div>
-                <p style={{ fontSize:34 }}>What We Do</p>
+          <div className="aboutTitle">
+              <p>What We Do</p>
+          </div>
+
+          <div id="aboutUsContent">
+            <div id="mission">
+              <p>Our mission is to create an inclusive and collaborative community around Computer Science and provide
+              resources and opportunities for all Oxy students
+              </p>
             </div>
+          </div>
 
-            <div id="about-us-content">
-              <div id="mission">
-                <p>Our mission is to create an inclusive and collaborative community around Computer Science and provide
-                resources and opportunities for all Oxy students
-                </p>
+          <div className="aboutTitle">
+            <p>Who We Are</p>
+          </div>
+
+          <div id="whomst">
+            <div id="photo-grid">
+              <div class="about-info">
+                <img src={placeholder}></img>
+                <div>Teagan Mucher, <i>Co-President</i></div>
               </div>
             </div>
-
-            <div>
-              <p style={{ fontSize:34 }}>Who We Are</p>
-            </div>
-
-            <div id="whomst">
-              <div id="photo-grid">
-                <div class="about-info">
-                  <img src={placeholder}></img>
-                  <div>Teagan Mucher, <i>Co-President</i></div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </section>
+          </div>
+        </div>
       </div>
     )
   }
