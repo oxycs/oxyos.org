a5c18c83d19bed1df84ee3dcd75ec57582c1c73a
Teagan Mucher <tjsmuch@gmail.com>
2019-08-27 20:51:08 -0700

    Added more about us content
    Added elements for each eboard member, fixed css

--- react-client/src/Components/AboutComponent/AboutUsPage.jsx
+++ react-client/src/Components/AboutComponent/AboutUsPage.jsx
@@ -24,11 +24,43 @@ class AboutUsPage extends Component {
             <p>Who We Are</p>
           </div>
 
-          <div id="whomst">
-            <div id="photo-grid">
+          <div class="whomst">
+            <div class="photo-grid">
               <div class="about-info">
-                <img src={placeholder}></img>
+                <img class="headshot" src={placeholder}></img>
                 <div>Teagan Mucher, <i>Co-President</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Alexis Hale, <i>Co-President</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Joey Rose, <i>CS Event Coordinator</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Tammie Oh, <i>Non-CS Event Coordinator</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Di Hu, <i>Head of Projects</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Scott Morris, <i>Public Relations</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Madeline Martell, <i>Network Outreach Coordinator</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Sophia Yang, <i>Public Relations</i></div>
+              </div>
+              <div class="about-info">
+                <img class="headshot" src={placeholder}></img>
+                <div>Patrick Bender, <i>Treasurer</i></div>
               </div>
             </div>
           </div>
