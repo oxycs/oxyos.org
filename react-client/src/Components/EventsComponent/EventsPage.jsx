785073a5a9cef96ad33c669a5ced0b682ae36e22
Unknown <halea@oxy.edu>
2019-08-30 23:59:31 -0700

    Added Upcoming and Past Events Sections
    Edited events page to have two columns: one of upcoming events and one of past events

--- react-client/src/Components/EventsComponent/EventsPage.jsx
+++ react-client/src/Components/EventsComponent/EventsPage.jsx
@@ -4,9 +4,29 @@ import './EventsPage.css';
 class EventsPage extends Component {
   render() {
     return (
-      <div id="events-page">
-        <p>No events yet. Check back soon!</p>
-      </div>
+      <section>
+        <section id="events">
+
+          <div>
+            <p style={{ fontSize:34 }}>Our Events</p>
+          </div>
+
+          <div id="container-events">
+
+            <div id="upcoming-events">
+              {/*Different way of declaring inline style with React*/}
+              <p style={{ fontSize:28 }}>Upcoming Events</p>
+              <p>hello there</p>
+            </div>
+            <div id="past-events">
+              <p style={{ fontSize:28 }}>Past Events</p>
+              <p>how are you doing</p>
+            </div>
+
+          </div>
+
+        </section>
+        </section>
     )
   }
 }
